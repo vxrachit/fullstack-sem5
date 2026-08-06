154809e8003937d0c4ca30475d49c8da0c8120f5
vxrachit <rachitv00@gmail.com>
2026-08-06 21:41:56 +0530

    add js basics and assignment2

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+// for(let i=0; i<arr.length ; i++){
+//     console.log(arr[i]**2);
+// }
+
+// var add = (a,b) => {
+//     console.log(a+b);
+// }
+// add(2,3);
+
+// var print = (arr) => {
+//     for(let i = 0 ; i<arr.length ; i++){
+//         console.log(arr[i]);
+//     }
+// }
+
+// print(arr);
